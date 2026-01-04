4c20d80ea54fd62b43945cb64e265d221848c40f
tlsdn0403@NoteBook <tlsdn0403@gmail.com>
2026-01-05 01:34:02 +0900

    문제해결 박스쌓기

--- baekjoon/389478.cpp
+++ baekjoon/389478.cpp
@@ -3,35 +3,37 @@
 #include<iostream>
 using namespace std;
 
-
 int solution(int n, int w, int num) {
-	int ans = 0;
-	vector<int> v(w);
-	for(int i = 0; i < n; i++) {
-		if ((n / w) % 2 != 0) {       //홀수
-			if (n % w > i) {
-				v[i] += (n / w) + 1;
-			} else {
-				v[i] += (n / w);
-			}
-		}
-		else {
-			if(n % w > i) {
-				v[i] += (n / w);
-			} else {
-				v[i] += (n / w) - 1;
-			}
-		}
-	}
-	int numHeight = w -num / w;
-	int numWidth;
-	if( num % 2 == 0) { 	//짝수
-		numWidth = w - (num % w);
-	}
-	else {
-		numWidth = (num % w)- 1;
-	}
-	return v[numWidth] - numHeight;
+    int numHeight = (num - 1) / w + 1;
+    int posInRow = (num - 1) % w;
+
+    // numWidth:  0-indexed 위치
+    int numWidth;
+    if (numHeight % 2 == 1) {
+        numWidth = posInRow;
+    }
+    else {
+        numWidth = w - 1 - posInRow;
+    }
+
+    int totalHeight = (n - 1) / w + 1;  // 최대 높이
+    int count = 0;
+
+    for (int h = numHeight; h <= totalHeight; h++) {
+        int boxNum; // 해당 높이에서 num이 위치할 수 있는 박스 번호 -> num위에 있는 박스
+
+        // 지금 높이가 홀수 일 때
+        if (h % 2 == 1) {
+            boxNum = (h - 1) * w + numWidth + 1;  //해당층 밑에 깔려있는 박스 수 + 그 층에서 몇 번째 박스인지 
+        }
+        else {
+            boxNum = (h - 1) * w + (w - 1 - numWidth) + 1;
+        }
+
+        if (boxNum <= n) count++;
+    }
+
+    return count;
 }
 
 int main() {
